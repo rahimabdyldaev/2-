89b7eca79fb0b10e2b546448055aa910308f4446
rahimabdyldaev <149590941+rahimabdyldaev@users.noreply.github.com>
2023-12-15 21:41:45 +0600

    Add files via upload

---
+++ src/Table.jsx
@@ -0,0 +1,26 @@
+
+const Table = ({data}) => {
+    return  <div>
+            {
+                JSON.stringify(data) == '{}' 
+                ? ''
+                : <table>
+                    <tr>
+                        <td>city</td>
+                        <td>{data.name}</td>
+                    </tr>
+                    <tr>
+                        <td>temp</td>
+                        <td>{data.main.temp}</td>
+                    </tr>
+                    <tr>
+                        <td>descr</td>
+                        <td>{data.weather[0].description}</td>
+                    </tr>
+                </table>
+            }
+        </div>;
+    
+};
+
+export default Table;
